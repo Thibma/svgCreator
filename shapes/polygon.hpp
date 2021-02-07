a9dbcf8b618e2c4c588b1224f6c272b4b750998a
roseeDuMatin <s_pelluet@hotmail.fr>
2021-02-07 22:22:01 +0100

    Polygon opérateurs

--- shapes/polygon.hpp
+++ shapes/polygon.hpp
@@ -11,19 +11,21 @@ private:
     int m_numbersOfPoints;
     std::vector<Point> m_points;
 
-// protected:
-//     void print(std::ostream& os) const{ 
-//     }
-//     friend std::ostream& operator<< (std::ostream& os, const Polygon& polygon){
-//         return os << polygon;
-//     }
-
 public:
     Polygon(int numbersOfPoints, std::vector<Point> points, int stroke, Color strokeColor, Color fillColor);
     virtual ~Polygon();
 
-    int getNumbersOfPoints();
-    std::vector<Point> getPoints();
+    int getNumbersOfPoints() const;
+    std::vector<Point> getPoints() const;
+
+    Polygon operator*(int multi);				 	  	  	  	  
+    Polygon operator+(int value);
 };
 
+
+Polygon operator*(const Polygon &p, int multi);
+Polygon operator*(int multi, const Polygon &p);
+
+Polygon operator+(const Polygon &p, int value);
+Polygon operator+(int value, const Polygon &p);
 #endif
